c98147a2e8ccdd1729effd6894569bf96c380713
SenkoSss <osypsopik@gmail.com>
2024-06-13 21:01:54 +0200

    Update player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300.00
+const SPEED = 200.00
 const JUMP_VELOCITY = -300.0
 const WALLJUMP_DISTANCE = 20
 
+var acceleration = 20
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var dodge_direction = 1;
 var spaces = 0
@@ -13,15 +14,14 @@ var start_position = 0
 
 @onready var ray_cast_2d_right = $RayCast2DRight
 @onready var ray_cast_2d_left = $RayCast2DLeft
+@onready var animation = $AnimatedSprite2D
 
 func _ready():
+	animation.play("idle")
 	ray_cast_2d_right.enabled = true
 	ray_cast_2d_left.enabled = true
 	start_position = position.x
 
-func _process(delta):
-	position.x += 5 * delta
-		
 
 func _physics_process(delta):
 	handle_gravity(delta)
@@ -43,8 +43,8 @@ func handle_jump(delta):
 		elif is_on_wall():
 			spaces = 0
 			if ray_cast_2d_left.is_colliding():
-				position.x += 50
-				position.y -= 50
+				velocity.x = 50
+				velocity.y -= 50
 			elif ray_cast_2d_right.is_colliding():
 				position.x -= 50
 				position.y -= 50
@@ -60,14 +60,21 @@ func handle_dodge(delta):
 
 func handle_wall_interactions(delta):
 	if (ray_cast_2d_left.is_colliding() or ray_cast_2d_right.is_colliding()) and is_on_wall() and not is_on_floor():
-		velocity.y *= 0.5
+		velocity.y *= 0.65
 	if Input.is_action_pressed("get_stopped_near_wall") and ray_cast_2d_left.is_colliding():
 		velocity.y = 0
 
 func handle_movement(delta):
 	var direction = Input.get_axis("go_left", "go_right")
+	if direction == 1:
+		animation.flip_h = false
+	elif direction == -1:
+		animation.flip_h = true
 	if direction:
 		dodge_direction = direction
-		velocity.x = direction * SPEED
+		if direction == 1:
+			velocity.x = min((velocity.x + acceleration)*direction,SPEED)
+		elif direction == -1:
+			velocity.x = max(velocity.x - acceleration,-SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = lerp(velocity.x,0.0,0.2)
